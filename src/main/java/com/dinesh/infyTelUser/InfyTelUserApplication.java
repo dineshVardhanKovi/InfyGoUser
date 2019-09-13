3980fc0c10a853af75986d19a25e661178f1cdd3
dineshVardhanKovi <dineshVardhanKovi@users.noreply.github.com>
2019-09-13 22:18:05 +0530

    project creation

---
+++ src/main/java/com/dinesh/infyTelUser/InfyTelUserApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.infyTelUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyTelUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyTelUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dinesh/infyTelUser/InfyTelUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dinesh.infyTelUser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfyTelUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
